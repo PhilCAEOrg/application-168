5f0c16af6208e8c3b6610389839c2ea658bf16bc
PhilCAEUser <mail@rwth>
2020-07-28 09:57:39 +0000

    Test

---
+++ get_ext_dependencies.sh
@@ -0,0 +1,7 @@
+mkdir dependencies
+cd dependenciesmkdir frontend && cd frontend
+git clone https://github.com/PhilCAEOrg/WidgetTest
+cd ..
+mkdir microservices && cd microservices
+git clone https://github.com/PhilCAEOrg/microservice-165
+cd ..
